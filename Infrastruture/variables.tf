6e7873bdbe1b6f195ff8b4b6b3c46ca1d567e5de
Vanessa Francis <vfrancis.francis@gmail.com>
2022-07-03 15:51:25 -0300

    Cria DataLake usando Terraforms

---
+++ Infrastruture/variables.tf
@@ -0,0 +1,12 @@
+variable "base_bucket_name" {
+  default = "datalakeigtitf"
+}
+
+variable "ambiente" {
+  default = "producao"
+}
+
+variable "numero_conta" {
+  default = "443822647869"
+}
+
---
+++ job_spark.py
@@ -0,0 +1,24 @@
+from pyspark.sql.functions import mean, max, min, col, count
+from pyspark.sql import SparkSession 
+
+spark = ( SparkSession.builder.appName("ExerciseSpark").getOrCreate())
+
+#Ler os dados do enem 2021
+enem = (
+    spark
+    .read
+    .format("csv")
+    .option("header", True)
+    .option("inferSchema", True)
+    .option("delimiter", ";")
+    .load("s3://datalake-igti-queen/raw-data/")
+) 
+
+(
+   enem 
+   .write
+   .mode("overwrite")
+   .format("parquet")
+   .partitionBy("year")
+   .save("s3://datalake-igti-queen/stanging/enem")
+)
