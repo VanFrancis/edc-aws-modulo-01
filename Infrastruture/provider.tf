65f2cf8b3aadc6ab842b52affd3da89c34e1f039
Vanessa Francis <vfrancis.francis@gmail.com>
2022-07-03 17:11:51 -0300

    refactor backend tf s3

---
+++ Infrastruture/provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+   region = var.aws_region
+}
+
+
+#Centralizar o controle de arquivo de controle de estado do terraform
+terraform {
+   backend "s3" {
+      bucket = "terraform-state-vanessa"
+      key = "state/igti/edc/mod1/terraform.tfstate"
+      region = "us-east-2"
+   }
+}
--- Infrastruture/variables.tf
+++ Infrastruture/variables.tf
@@ -1,12 +1,3 @@
-variable "base_bucket_name" {
-  default = "datalakeigtitf"
+variable "aws_region" {
+  default = "us-east-2"
 }
-
-variable "ambiente" {
-  default = "producao"
-}
-
-variable "numero_conta" {
-  default = "443822647869"
-}
-
